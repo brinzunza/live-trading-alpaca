e57c2289e7a2ee22a02e344bdf61b23ecb0148d0
Bruno Inzunza <bruno.inzunza24@gmail.com>
2024-09-25 12:27:41 -0500

    api testing

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # live-trading-alpaca
- 
+This is a basic trading bot that interacts with the Alpaca API to buy and sell stocks or cryptocurrencies (like Bitcoin). The bot is built using Python and the alpaca-trade-api library to execute trades. This example is set up to trade Bitcoin (BTC/USD) on the Alpaca paper trading account.
+
+## Features
+
+	•	Buy and Sell Orders: You can submit market buy and sell orders for any symbol supported by Alpaca (e.g., stocks, crypto).
+	•	Alpaca API Integration: Uses Alpaca’s REST API for submitting orders.
+	•	Paper Trading: Interact with Alpaca’s paper trading system for testing without real money.
---
+++ liveTrading.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from sklearn.preprocessing import MinMaxScaler
+from datetime import datetime, timedelta
+import time
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import REST, TimeFrame
+
+# Alpaca API credentials
+ALPACA_API_KEY = 'PKWG6BM2U94TPRYJSXSY'
+ALPACA_SECRET_KEY = 'tMuzBSWeqHUfjyb5f0hZtLXowfKdmeurF1CmiHAS'
+ALPACA_BASE_URL = 'https://paper-api.alpaca.markets'  # Use this for paper trading
+
+# Initialize Alpaca API
+api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_BASE_URL, api_version='v2')
+
+def buy(symbol, qty):
+    api.submit_order(
+        symbol=symbol,
+        qty=qty,
+        side='buy',
+        type='market',
+        time_in_force='gtc'
+    )
+    return(f"Bought {qty} shares of {symbol}")
+
+def sell(symbol, qty):
+    api.submit_order(
+        symbol=symbol,
+        qty=qty,
+        side='sell',
+        type='market',
+        time_in_force='gtc'
+    )
+    return(f"Sold {qty} shares of {symbol}")
+
+if __name__ == "__main__":
+    buy('BTC/USD', 1)
